083e7a8e90e9ba34ddaa5f3837b42c6a0608a33c
Forpgm <phm.giamy@gmail.com>
2024-06-11 10:01:33 +0700

    update main code to use recursion

--- src/main/java/com/forpgm/mathutil/core/MathUtility.java
+++ src/main/java/com/forpgm/mathutil/core/MathUtility.java
@@ -17,13 +17,18 @@ public class MathUtility {
             throw new IllegalArgumentException("Invalid n. n must be between 0 - 20");
 
         }
-        if (n == 0) {
+        if (n == 0 || n == 1) {
             return 1;
         }
-        for (int i = 1; i <= n; i++) {
-            product *= i;
-        }
 
-        return product;
+        return n * getFactorial(n - 1);
     }
 }
+
+//5! = 1.2.3.4.5
+//5! = 5.4!
+//4! = 1.2.3.4
+//4! = 4.3!
+//n! = n.(n-1)! | công thức đệ qui - recursion
+
+//búp bê người nga
